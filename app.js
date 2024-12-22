a5a4e25da1de982942aa6e5bccf58513893f83b7
Rajesh kumar <147221172+rajeshk1909@users.noreply.github.com>
2024-12-22 14:29:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 // Configure CORS
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://shopsphere-fe.vercel.app",
+  "https://buyspot.vercel.app",
   "https://shopsphere-fe.onrender.com",
 ]
 
